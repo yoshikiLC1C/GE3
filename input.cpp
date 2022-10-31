bfe71ea561fd92ab89c8bfcff7da311cbf512902
itouyoshiki <k021g1467@g.neec.ac.jp>
2022-10-31 13:28:00 +0900

    02_03

--- input.cpp
+++ input.cpp
@@ -12,7 +12,6 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd) {
     HRESULT result;
    
     // DirectInputの初期化
-    ComPtr<IDirectInput8> directInput;
     result = DirectInput8Create(
         hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
     assert(SUCCEEDED(result));
@@ -31,12 +30,33 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd) {
 
 void Input::Update() {
 
+    // 前回のキー入力を保存
+    memcpy(keyPre, key, sizeof(key));
+
     // キーボード情報の取得開始
     keyboard->Acquire();
     // 全キーの入力状態を取得する
-    BYTE key[256] = {};
     keyboard->GetDeviceState(sizeof(key), key);
   
 
+}
+
+bool Input::PushKey(BYTE keyNumber) {
+    // 指定キーを押していればtrueを返す
+    if (key[keyNumber]) {
+        return true;
+    }
+
+    return false;
+}
+
+bool Input::TriggerKey(BYTE keyNumber)
+{
+    // 指定キーを押していたら
+    if (keyPre[keyNumber] == 0 && key[keyNumber]) {
+        return true;
+    }
+
+    return false;
 }
 
--- input.h
+++ input.h
@@ -15,10 +15,32 @@ public: // メンバ関数
 	void Initialize(HINSTANCE hinstance,HWND hwnd);
 	void Update();
 
+	/// <summary>
+	/// 入力
+	/// </summary>
+	/// <param name="keyNumber">キー番号(DIK_0等)</param>
+	/// <returns>押されているか</returns>
+	bool PushKey(BYTE keyNumber);
+
+	/// <summary>
+	/// キーのトリガーをチェック
+	/// </summary>
+	/// <param name="keyNumber">キー番号(DIK_0等)</param>
+	/// <returns>押されているか</returns>
+	bool TriggerKey(BYTE keyNumber);
 
 
 private: // メンバ変数
-// キーボードのデバイス
+	// キーボードのデバイス
 	ComPtr<IDirectInputDevice8> keyboard;
+
+	// DirectInputの初期化
+	ComPtr<IDirectInput8> directInput;
+
+	// 全キーの状態
+	BYTE key[256] = {};
+
+	// 前回の全キー状態
+	BYTE keyPre[256] = {};
 };
 
